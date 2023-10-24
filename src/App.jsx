d3d78376baf65275ac36903c1f1c9ef685270504
louisa ng <louisa825@yahoo.com>
2023-10-23 22:19:42 -0700

    wip

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
-import reactLogo from "./assets/react.svg";
 import "./App.css";
 import Article from "./Article";
+import secImg from "./images/sec1.jpg";
+import secretarybirdText from "./texts/sec.js";
 
 function App() {
   return Article({
     title: "Secretarybird",
-    image: reactLogo,
-    description: "This is a description",
+    image: secImg,
+    description: secretarybirdText,
     author: "John Doe",
     date: "2021-01-01",
+    backgroundColor: "#697827",
   });
 }
 
